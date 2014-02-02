6750cbacde08ba8af66a4336e995a93ede6c44c5
Erik Ringsmuth <erik.ringsmuth@gmail.com>
2014-02-02 17:27:53 -0600

    fix IE8 code formatting

--- js/util/utilities.js
+++ js/util/utilities.js
@@ -5,19 +5,25 @@ define([
 ], function($, prettify) {
   'use strict';
 
-  return {
-    html5: function html5() {
-      return ('querySelector' in document && 'localStorage' in window && 'addEventListener' in window);
-    },
+  var utilities = {
+    // Test if the browser supports common HTML5 DOM APIs
+    html5: 'querySelector' in document && 'localStorage' in window && 'addEventListener' in window,
 
+    // Format code for readability. This will look for every code block in the element and format it.
     formatCode: function formatCode(element) {
-      $(element).find('code').each(function() {
-        var $el = $(this);
-        if (!$el.hasClass('pln')) {
-          $el.html(prettify.prettyPrintOne($el.html()));
-        }
-      });
+      // Google doesn't even support IE8...
+      if (utilities.html5) {
+        $(element).find('code').each(function() {
+          var $el = $(this);
+          // If the code block has class 'pln' don't format the code
+          if (!$el.hasClass('pln')) {
+            $el.html(prettify.prettyPrintOne($el.html()));
+          }
+        });
+      }
       return element;
     }
   };
+
+  return utilities;
 });
